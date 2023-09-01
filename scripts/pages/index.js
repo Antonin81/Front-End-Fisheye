a0b0596127cef1146e4a27368c8df2ef87d6885c
Antonin81 <antonin.pidet@gmail.com>
2023-09-01 17:35:18 +0200

    Ajout des images des photographes et import des données

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,15 +1,16 @@
     async function getPhotographers() {
 
-        fetch("../../data/photographer.json")
+        let photographersTest=[];
+
+        await fetch("data/photographers.json")
         .then((response)=>{
-            console.log("fojhbl");
             return response.json();
         })
         .then((data)=>{
-            console.log('data');
-            console.log(data);
+            console.log(data.photographers);
+            photographersTest.push(data.photographers);
+            console.log(photographersTest);
         }) 
-
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
         let photographers = [
@@ -34,13 +35,13 @@
         ]
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: photographersTest})
     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
+        photographers[0].forEach((photographer) => {
+            console.log(photographer);
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
