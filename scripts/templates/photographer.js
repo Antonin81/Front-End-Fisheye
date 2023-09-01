cc73cbbab6b36336625da3cbdf8c6ea03c3c245a
Antonin81 <antonin.pidet@gmail.com>
2023-09-01 15:26:44 +0200

    Ajout des données manquantes et de quelques roles

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,41 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, price, country, city, tagline } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
         const article = document.createElement( 'article' );
+
+        const link = document.createElement( 'a' );
+        link.setAttribute("href", "#");
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
+
+        const location = document.createElement( 'p' );
+        location.textContent=city+", "+country;
+        location.classList.add("location");
+
+        const taglineParagraph = document.createElement( 'p' );
+        taglineParagraph.textContent=tagline;
+        taglineParagraph.classList.add("tagline");
+
+        const priceParagraph = document.createElement( 'p' );
+        priceParagraph.textContent=price+"€/jour";
+        priceParagraph.classList.add("price");
+
+        article.appendChild(location);
+        article.appendChild(taglineParagraph);
+        article.appendChild(priceParagraph);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
